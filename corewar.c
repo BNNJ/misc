33f9f86095a9636107eb5dd2de32a0a793155a07
BNNJ <32491514+BNNJ@users.noreply.github.com>
2019-04-14 19:51:19 +0200

    Update corewar.c

--- corewar.c
+++ corewar.c
@@ -1,11 +1,13 @@
 #include "test.h"
 #include <stdint.h>
 
-# define MEM_SIZE				(4*1024)
+# define MEM_SIZE				(4 * 1024)
 # define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
 # define PROG_NAME_LENGTH		(128)
 # define COMMENT_LENGTH			(2048)
 # define COREWAR_EXEC_MAGIC		0xea83f3
+
+# define MAX_PLAYERS			4
 
 # define OPT_A 0
 # define OPT_B 1
@@ -19,12 +21,14 @@ typedef struct	s_champ
 	char			name[PROG_NAME_LENGTH + 8];
 	char			comment[COMMENT_LENGTH + 4];
 	char			code[CHAMP_MAX_SIZE];
+	int				nb;
 	struct s_champ	*next;
 }				t_champ;
 
 typedef struct	s_vm
 {
 	int				opt;
+	uint8_t			mem[MEM_SIZE];
 	int				nb_player;
 	t_champ			*champs;
 }				t_vm;
@@ -43,10 +47,12 @@ int		ft_swap_int32(int nb)
 		| (nb & 0x000000ff) << 24);
 }
 
+/* ####################### PARSE ARG #####################  */
+
 static int	get_long_opt(char *arg, t_vm *vm)
 {
 	int		i;
-	char	*opt_table[] = {"axx", "bxxx", "cxxx", "dxxx", "exxx", NULL};
+	char	*opt_table[] = {"viewer", "debug", "help", NULL};
 
 	i = 0;
 	while (opt_table[i] && ft_strcmp(opt_table[i], arg))
@@ -69,7 +75,7 @@ static int	get_opt(char *arg, t_vm *vm)
 		return (get_long_opt(arg + 1, vm));
 	while (*arg)
 	{
-		if ((opt = ft_findchar("abcde", *arg)) >= 0)
+		if ((opt = ft_findchar("vgh", *arg)) >= 0)
 			vm->opt |= 1 << opt;
 		else
 		{
@@ -80,7 +86,31 @@ static int	get_opt(char *arg, t_vm *vm)
 	return (1);
 }
 
-static int	get_champ(char *arg, t_vm *vm)
+static t_champ	*new_champ(char *arg, int fd)
+{
+	t_champ	*champ;
+	off_t	file_size;
+
+	if (!(champ = ft_memalloc(sizeof(t_champ))))
+	{
+		ft_dprintf(2, "malloc error\n");
+		return (NULL);
+	}
+	if (read(fd, &champ->magic,
+		sizeof(t_champ) - sizeof(void*) - sizeof(int)) <= 0)
+	{
+		ft_dprintf(2, "corewar: error reading file %s\n", arg);
+		return (NULL);
+	}
+	if ((champ->magic = ft_swap_int32(champ->magic)) != COREWAR_EXEC_MAGIC)
+	{
+		ft_dprintf(2, "corewar: invalid magic number for %s\n", arg);
+		return (NULL);
+	}
+	return (champ);
+}
+
+static int	get_champ(char *arg, t_vm *vm, int player_number)
 {
 	int		fd;
 	t_champ	*champ;
@@ -90,30 +120,26 @@ static int	get_champ(char *arg, t_vm *vm)
 		ft_dprintf(2, "corewar: can't open file: %s\n", arg);
 		return (0);
 	}
-	if (!(champ = malloc(sizeof(t_champ))))
-		return (0);
-	if (read(fd, &champ->magic, sizeof(t_champ) - sizeof(void*)) <= 0)
+	if ((champ = new_champ(arg, fd)) != NULL)
 	{
-		ft_dprintf(2, "corewar: error reading file %s\n", arg);
-		return (0);
-	}
-	if ((champ->magic = ft_swap_int32(champ->magic)) != COREWAR_EXEC_MAGIC)
-	{
-		ft_dprintf(2, "corewar: invalid magic number\n");
-		return (0);
+		champ->nb = player_number;
+		champ->next = vm->champs;
+		vm->champs = champ;
+		++vm->nb_player;
+		close(fd);
+		return (1);
 	}
 	close(fd);
-	champ->next = vm->champs;
-	vm->champs = champ;
-	++vm->nb_player;
-	return (1);
+	return (0);
 }
 
 int			parse_args(int argc, char **argv, t_vm *vm)
 {
 	int		i;
+	int		player_number;
 
 	i = 0;
+	player_number = -1;
 	while (++i < argc)
 	{
 		if (argv[i][0] == '-')
@@ -123,26 +149,71 @@ int			parse_args(int argc, char **argv, t_vm *vm)
 		}
 		else
 		{
-			if (!get_champ(argv[i], vm))
+			if (!get_champ(argv[i], vm, player_number))
 				return (0);
+			--player_number;
 		}
 	}
 	return (1);
 }
 
+/* ########################## INIT #######################  */
+
+int			load_champ(t_vm *vm, t_champ *ch)
+{
+	int	i;
+	int	j;
+
+	i = (MEM_SIZE / vm->nb_player) * (ch->nb + vm->nb_player);
+	j = 0;
+	while (j < CHAMP_MAX_SIZE)
+	{
+		vm->mem[i] = ch->code[j];
+		++i;
+		++j;
+	}
+	return (1);
+}
+
+int			init(t_vm *vm)
+{
+	t_champ	*tmp;
+
+	if (vm->nb_player > MAX_PLAYERS || vm->nb_player < 2)
+	{
+		ft_dprintf(2, "corewar: incorrect number of players\n");
+		return (0);
+	}
+	tmp = vm->champs;
+	while (tmp)
+	{
+		load_champ(vm, tmp);
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
+/* ########################## MAIN #######################  */
+
 int			main(int argc, char **argv)
 {
 	t_vm	vm;
 
-	vm = (t_vm){0, 0, NULL};
+	vm = (t_vm){0, {0}, 0, NULL};
 	if (argc >= 2)
 	{
-		parse_args(argc, argv, &vm);
+		if (!(parse_args(argc, argv, &vm)))
+			return (EXIT_FAILURE);
+		if (!(init(&vm)))
+			return (EXIT_FAILURE);
+		int	fd;
+		fd = open("output_test", O_WRONLY | O_CREAT);
+		write(fd, vm.mem, MEM_SIZE);
+		close(fd);
+		ft_printf("%x\n", vm.champs->magic);
+		ft_printf("%s\n", vm.champs->name);
+		ft_printf("%s\n", vm.champs->comment);
+//		ft_printf("%s\n", vm.champs->code);
 	}
-//	write(1, vm.champs->name, PROG_NAME_LENGTH + 10);
-	ft_printf("%x\n", vm.champs->magic);
-	ft_printf("%s\n", vm.champs->name);
-	ft_printf("%s\n", vm.champs->comment);
-	ft_printf("%s\n", vm.champs->code);
 	return (EXIT_SUCCESS);
 }
